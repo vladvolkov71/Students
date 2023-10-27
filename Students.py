40bb98d91bd7254400445d212e284bc83dae7443
“Vlad <vladvolkov71@gmail.com>
2023-10-27 10:29:13 +0300

    Recall __STR__

--- Students.py
+++ Students.py
@@ -3,12 +3,25 @@ class Student:
         self.name = name
         self.surname = surname
         self.gender = gender
-        self.finished_courses = []
+        self.courses_end = []
         self.courses_in_progress = []
-        self.grades = {}
+        self.grades_stud = {}
 
-        def add_courses(self, course_name):
-            self.finished_courses.append(course_name)
+    def __str__(self):
+        return (f"Имя: {self.name}\n"
+                f"Фамилия: {self.surname}\n"
+                f"Средняя оценка за домашние задания:\n"
+                f"Курсы в процессе изучения: {','.join(self.courses_in_progress)}\n"
+                f"Завершенные курсы: {','.join(self.courses_end)}")
+
+    def rate_lect(self, lect, course, grade):
+        if isinstance(lect, Lecturer) and course in self.courses_in_progress and course in lect.courses_attached:
+            if course in lect.grades_lect:
+                lect.grades_lect[course] += [grade]
+            else:
+                lect.grades_lect[course] = [grade]
+        else:
+            return 'Ошибка'
 
 
 class Mentor:
@@ -17,47 +30,33 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
-
 
 class Lecturer(Mentor):
-    pass
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades_lect = {}
+
+    def __str__(self):
+        return (f"Имя: {self.name}\n"
+                f"Фамилия: {self.surname}\n"
+                f"Средняя оценка за лекции:\n")
 
 
 class Reviewer(Mentor):
-    pass
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades_stud:
+                student.grades_stud[course] += [grade]
+            else:
+                student.grades_stud[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    def __str__(self):
+        return (f"Имя: {self.name}\n"
+                f"Фамилия: {self.surname}\n")
 
 
-# ___________1___________
-# У Reviewer реализовать возможность выставлять студентам оценки за домашние задания.
-# У Student реализуйте метод выставления оценок лекторам
-# (оценки по 10-балльной шкале, хранятся в атрибуте-словаре у Lecturer, в котором ключи – названия курсов,
-# а значения – списки оценок). Лектор при этом должен быть закреплен за тем курсом, на который записан студент.
-# ___________2___________
-# Перегрузите магический метод __str__ у всех классов.
-# У проверяющих он должен выводить информацию в следующем виде:
-# print(some_reviewer)
-# Имя: Some
-# Фамилия: Buddy
-# У лекторов:
-# print(some_lecturer)
-# Имя: Some
-# Фамилия: Buddy
-# Средняя оценка за лекции: 9.9
-# А у студентов так:
-# print(some_student)
-# Имя: Ruoy
-# Фамилия: Eman
-# Средняя оценка за домашние задания: 9.9
-# Курсы в процессе изучения: Python, Git
-# Завершенные курсы: Введение в программирование.
 # Реализуйте возможность сравнивать (через операторы сравнения) между собой лекторов по средней оценке за лекции
 # и студентов по средней оценке за домашние задания.
 # __________3____________
@@ -70,11 +69,22 @@ class Reviewer(Mentor):
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_lecturer = Lecturer('Ivan', 'Petrov')
+cool_lecturer.courses_attached += ['Python']
 
-print(best_student.grades)
+cool_reviewer.rate_hw(best_student, 'Python', 9)
+cool_reviewer.rate_hw(best_student, 'Python', 7)
+cool_reviewer.rate_hw(best_student, 'Python', 5)
+
+best_student.rate_lect(cool_lecturer, 'Python', 10)
+best_student.rate_lect(cool_lecturer, 'Python', 8)
+best_student.rate_lect(cool_lecturer, 'Python', 6)
+
+print(cool_lecturer.grades_lect)
+print(best_student.grades_stud)
+print(best_student)
+print(cool_lecturer)
+print(cool_lecturer)
